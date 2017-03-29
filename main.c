b3d97ffbfa686025bb9091a643673756734b46fa
vjbd4349 <vjbd4349@r-dhcp-50-c4-34-6b-55-d1-b6.rd.francetelecom.fr>
2017-03-29 17:26:35 +0200

    fichier de test

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "externe.h"
+
+
+
+int main()
+{
+    
+   service *s= newService();
+    
+    s->ipaddr = "161.105.178.178";
+    s->job = "authentification";
+    s->name = "serveur authentification";
+    s->port = 1010;
+
+    affiche_info(s);
+    return 0;
+}
